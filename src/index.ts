cc4ca09b812ce08530fec029d1ac81f5c88d3695
Lixqa <53451629+Lixqa@users.noreply.github.com>
2025-10-08 19:27:27 +0200

    Add z to export

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { Config } from './lib/typings/types';
 export { defineSchema } from './lib/define-schema';
 export { defineConfig } from './lib/define-config';
 export { createApp } from './lib/create-app';
+export { z } from 'zod';
 
 function catchApiKill(e: unknown) {
   if (typeof e == 'string' && e == 'API_KILL') return;

ca69ff91e9f108ca847bf55751f467193e0cea34
Lixqa <53451629+Lixqa@users.noreply.github.com>
2025-10-17 08:47:57 +0200

    Expose StatusCodes from index

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export { defineSchema } from './lib/define-schema';
 export { defineConfig } from './lib/define-config';
 export { createApp } from './lib/create-app';
 export { z } from 'zod';
+export { StatusCodes as HttpStatus } from 'http-status-codes';
 
 function catchApiKill(e: unknown) {
   if (typeof e == 'string' && e == 'API_KILL') return;

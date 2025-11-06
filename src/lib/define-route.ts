79345ffad183c07989111d72b0f5c12319e6818c
Lixqa <53451629+Lixqa@users.noreply.github.com>
2025-11-06 10:15:49 +0100

    Fix shared method type

--- src/lib/define-route.ts
+++ src/lib/define-route.ts
@@ -1,24 +1,23 @@
-import { RouteDefinition, SchemaDefinition } from './typings/types';
+import { RouteDefinition } from './typings/types';
 
 // Helper type to extract the return type of shared.pre
 type InferShared<T> = T extends {
   shared: { pre: (...args: any[]) => infer R };
 }
-  ? R extends Promise<infer U>
-    ? U
-    : R
+  ? Awaited<R>
   : undefined;
 
 export function defineRoute<
-  Schema extends SchemaDefinition = object,
-  TAuth = any,
-  TServices = undefined,
-  Route extends RouteDefinition<
-    Schema,
-    TAuth,
-    TServices,
-    any
-  > = RouteDefinition<Schema, TAuth, TServices, any>,
->(route: Route): RouteDefinition<Schema, TAuth, TServices, InferShared<Route>> {
+  const Route extends RouteDefinition<any, any, any, any>,
+>(
+  route: Route,
+): RouteDefinition<
+  Route extends RouteDefinition<infer V, any, any, any> ? V : any,
+  Route extends RouteDefinition<any, infer TAuth, any, any> ? TAuth : any,
+  Route extends RouteDefinition<any, any, infer TServices, any>
+    ? TServices
+    : any,
+  InferShared<Route>
+> {
   return route as any;
 }

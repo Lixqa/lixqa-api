d91c08ad251f30b781c199cb0fde98993e2dea08
Lixqa <53451629+Lixqa@users.noreply.github.com>
2025-11-06 10:01:02 +0100

    Fix shared method type

--- src/lib/define-route.ts
+++ src/lib/define-route.ts
@@ -1,12 +1,32 @@
-import { RouteDefinition, SchemaDefinition } from './typings/types';
+import {
+  RouteDefinition,
+  SchemaDefinition,
+  SharedPreAPI,
+} from './typings/types';
 
 export function defineRoute<
   Schema extends SchemaDefinition = object,
   TAuth = any,
   TServices = undefined,
-  TShared = undefined,
+  TShared = unknown,
 >(
-  route: RouteDefinition<Schema, TAuth, TServices, TShared>,
-): RouteDefinition<Schema, TAuth, TServices, TShared> {
+  route: RouteDefinition<Schema, TAuth, TServices, TShared> & {
+    shared: {
+      pre: (
+        api: SharedPreAPI<Schema, TAuth, TServices>,
+      ) => TShared | Promise<TShared>;
+    };
+  },
+): RouteDefinition<Schema, TAuth, TServices, TShared>;
+
+export function defineRoute<
+  Schema extends SchemaDefinition = object,
+  TAuth = any,
+  TServices = undefined,
+>(
+  route: RouteDefinition<Schema, TAuth, TServices, undefined>,
+): RouteDefinition<Schema, TAuth, TServices, undefined>;
+
+export function defineRoute(route: any): any {
   return route;
 }

c58bb2e9d1e9a7c2f63439bfda33e58dae8bc7fd
Lixqa <53451629+Lixqa@users.noreply.github.com>
2025-12-09 00:52:51 +0100

    Add proper __client__ logic

--- src/internal-routes/__client__.ts
+++ src/internal-routes/__client__.ts
@@ -5,7 +5,16 @@ const route: RouteDefinition = {
     unauthed: true, // Allow unauthenticated access
   },
   GET: async (api) => {
-    api.send('test');
+    const routes = api.server.routes.items.map((route) => {
+      return {
+        path: route.path,
+        settings: route.settingsFor(),
+        methods: route.methods,
+        schema: route.schema?.file,
+      };
+    });
+
+    api.send(routes);
   },
 };
 
